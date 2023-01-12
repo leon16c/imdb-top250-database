2473913e9164494844f554930510d43a317403af
leon16c <leon16c03@gmail.com>
2023-01-12 19:44:05 +0100

    Hello

--- search_top250_by_genres.py
+++ search_top250_by_genres.py
@@ -1,16 +1,17 @@
-#establish connection to db
+# establish connection to db
 import sqlite3
+
 conn = sqlite3.connect('databases/movies_top_250.db')
 cursor = conn.cursor()
 
-#userinput
+# userinput
 genre_list = []
 line = input()
 while line != "":
     genre_list.append(line)
     line = input()
 
-#query
+# query
 query = 'SELECT DISTINCT movies.localized_title FROM movies INNER JOIN movie_genres ON movies.id = movie_genres.movie_id INNER JOIN genres ON movie_genres.genre_id = genres.id'
 placeholders = ' WHERE genres.name = ?'
 values = (genre_list[0],)
@@ -21,14 +22,14 @@ for i in range(1, len(genre_list)):
 query += placeholders
 cursor.execute(query, values)
 
-#dumb code to sanitize strings
+# dumb code to sanitize strings
 fetched = cursor.fetchone()
 while fetched is not None:
     fetched = str(fetched)
-    fetched = fetched.replace('\"', '').replace('(', '').replace(',)', '') if '\"' in fetched else fetched.replace('\'', '').replace('(', '').replace(',)', '')
+    fetched = fetched.replace('\"', '').replace('(', '').replace(',)', '') if '\"' in fetched else fetched.replace('\'','').replace('(', '').replace(',)', '')
     print(fetched)
     fetched = cursor.fetchone()
 
-#close connection to db
+# close connection to db
 cursor.close()
 conn.close()
